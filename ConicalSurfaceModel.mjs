c39e858d29a30a7adbcb6c767a5907d2afcad115
Mykola Stepanenko <mykola.stepanenko202@gmail.com>
2024-12-07 18:46:17 +0200

    PA3

--- ConicalSurfaceModel.mjs
+++ ConicalSurfaceModel.mjs
@@ -217,7 +217,7 @@ export default function ConicalSurfaceModel(gl, uSteps, vSteps, L, T, B)  {
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, this.indices, gl.STATIC_DRAW);
     }
 
-    draw(shaderProgram) {
+    this.draw = function (shaderProgram) {
         const gl = this.gl;
 
         // Positions
