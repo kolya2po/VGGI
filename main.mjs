731a02eb9f957a8bf7e9c4a1d0a0ac7e874b3c53
Mykola Stepanenko <mykola.stepanenko202@gmail.com>
2024-12-22 18:11:21 +0200

    CGW

--- main.mjs
+++ main.mjs
@@ -7,44 +7,74 @@ let surface;
 let shProgram;
 let spaceball;
 let zoom;
-let uSlider, vSlider;
-
-let lightAngle = 0.0;
+let uSlider, vSlider, scaleSlider;
 
 let diffuseTexture, specularTexture, normalTexture;
 let texturesLoaded = 0;
 
-let uRotationCenter = [0.5, 0.5];
-let uRotationAngle = 0.0;
+let lightAngle = 0.0;
 
-function ShaderProgram(name, program) {
-    this.name = name;
+let highlightU = 0.5;
+let highlightV = 0.5;
+const highlightStep = 0.05;
+
+let texScaleValue = 1.0;
+
+function ShaderProgram(program) {
     this.prog = program;
 
-    this.iAttribVertex = -1;
-    this.iAttribNormal = -1;
-    this.iAttribUV = -1;
-    this.iAttribTangent = -1;
+    this.iAttribVertex    = -1;
+    this.iAttribNormal    = -1;
+    this.iAttribUV        = -1;
+    this.iAttribTangent   = -1;
     this.iAttribBitangent = -1;
-    this.iColor = -1;
-    this.iModelViewProjectionMatrix = -1;
-    this.iModelViewMatrix = -1;
-    this.iNormalMatrix = -1;
-    this.iLightPos = -1;
-    this.iKa = -1;
-    this.iKd = -1;
-    this.iKs = -1;
-    this.iShininess = -1;
-    this.iDiffuseTex = -1;
-    this.iSpecularTex = -1;
-    this.iNormalTex = -1;
 
-    this.iRotationAngle = -1;
-    this.iRotationCenter = -1;
+    this.iModelViewProjectionMatrix = -1;
+    this.iModelViewMatrix           = -1;
+    this.iNormalMatrix              = -1;
+    this.iColor                     = -1;
+    this.iLightPos                  = -1;
+    this.iKa                        = -1;
+    this.iKd                        = -1;
+    this.iKs                        = -1;
+    this.iShininess                 = -1;
+    this.iDiffuseTex                = -1;
+    this.iSpecularTex               = -1;
+    this.iNormalTex                 = -1;
+
+    this.iTexScale   = -1;
+    this.iTexPivot   = -1;
+
+    this.iHighlightUV = -1;
 
     this.Use = function() {
         gl.useProgram(this.prog);
     };
+}
+
+function createProgram(gl, vShaderSrc, fShaderSrc) {
+    let vsh = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vsh, vShaderSrc);
+    gl.compileShader(vsh);
+    if (!gl.getShaderParameter(vsh, gl.COMPILE_STATUS)) {
+        throw new Error("Error in vertex shader:\n" + gl.getShaderInfoLog(vsh));
+    }
+
+    let fsh = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fsh, fShaderSrc);
+    gl.compileShader(fsh);
+    if (!gl.getShaderParameter(fsh, gl.COMPILE_STATUS)) {
+        throw new Error("Error in fragment shader:\n" + gl.getShaderInfoLog(fsh));
+    }
+
+    let prog = gl.createProgram();
+    gl.attachShader(prog, vsh);
+    gl.attachShader(prog, fsh);
+    gl.linkProgram(prog);
+    if (!gl.getProgramParameter(prog, gl.LINK_STATUS)) {
+        throw new Error("Link error in program:\n" + gl.getProgramInfoLog(prog));
+    }
+    return prog;
 }
 
 function loadTexture(url) {
@@ -52,10 +82,7 @@ function loadTexture(url) {
     texture.image = new Image();
     texture.image.onload = function() {
         gl.bindTexture(gl.TEXTURE_2D, texture);
-        gl.texImage2D(
-            gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE,
-            texture.image
-        );
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);
         gl.generateMipmap(gl.TEXTURE_2D);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
         gl.bindTexture(gl.TEXTURE_2D, null);
@@ -63,122 +90,6 @@ function loadTexture(url) {
     };
     texture.image.src = url;
     return texture;
-}
-
-function draw() {
-    if (texturesLoaded < 3) {
-        requestAnimationFrame(draw);
-        return;
-    }
-
-    gl.clearColor(0,0,0,1);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    let projection = m4.perspective(Math.PI/8, 1, 8, 50);
-    let modelView = spaceball.getViewMatrix();
-
-    let rotX = m4.xRotation(270 * Math.PI / 180);
-    modelView = m4.multiply(rotX, modelView);
-
-    let rotY = m4.yRotation(45 * Math.PI / 180);
-    modelView = m4.multiply(rotY, modelView);
-
-    let translateToPointZero = m4.translation(0, 0, -15/zoom);
-    modelView = m4.multiply(translateToPointZero, modelView);
-
-    // Update light angle
-    lightAngle += 0.01; // rotate the light around Y-axis
-    let lx = 5.0 * Math.cos(lightAngle);
-    let lz = 5.0 * Math.sin(lightAngle);
-    let ly = 4.0;
-    let lightPos = [lx, ly, lz];
-
-    // Transform light into eye space:
-    let lightEye = m4.transformPoint(modelView, lightPos);
-
-    let modelViewProjection = m4.multiply(projection, modelView);
-
-    let normalMatrix = m4.inverse(modelView);
-    normalMatrix = m4.transpose(normalMatrix);
-    let normalMatrix3 = [
-        normalMatrix[0], normalMatrix[1], normalMatrix[2],
-        normalMatrix[4], normalMatrix[5], normalMatrix[6],
-        normalMatrix[8], normalMatrix[9], normalMatrix[10]
-    ];
-
-    shProgram.Use();
-    gl.uniformMatrix4fv(shProgram.iModelViewProjectionMatrix, false, modelViewProjection);
-    gl.uniformMatrix4fv(shProgram.iModelViewMatrix, false, modelView);
-    gl.uniformMatrix3fv(shProgram.iNormalMatrix, false, normalMatrix3);
-
-    // Set material and light
-    gl.uniform4fv(shProgram.iColor, [1,1,1,1]);
-    gl.uniform3fv(shProgram.iLightPos, lightEye);
-    gl.uniform3fv(shProgram.iKa, [0.2,0.2,0.2]);
-    gl.uniform3fv(shProgram.iKd, [0.8,0.8,0.7]);
-    gl.uniform3fv(shProgram.iKs, [1.0,1.0,1.0]);
-    gl.uniform1f(shProgram.iShininess, 50.0);
-
-    // Bind textures
-    gl.activeTexture(gl.TEXTURE0);
-    gl.bindTexture(gl.TEXTURE_2D, diffuseTexture);
-    gl.uniform1i(shProgram.iDiffuseTex, 0);
-
-    gl.activeTexture(gl.TEXTURE1);
-    gl.bindTexture(gl.TEXTURE_2D, specularTexture);
-    gl.uniform1i(shProgram.iSpecularTex, 1);
-
-    gl.activeTexture(gl.TEXTURE2);
-    gl.bindTexture(gl.TEXTURE_2D, normalTexture);
-    gl.uniform1i(shProgram.iNormalTex, 2);
-
-    gl.uniform1f(shProgram.iRotationAngle, uRotationAngle);
-    gl.uniform2f(shProgram.iRotationCenter, uRotationCenter[0], uRotationCenter[1]);
-
-    surface.draw(shProgram);
-
-    requestAnimationFrame(draw);
-}
-
-function initGL() {
-    let prog = createProgram(gl, vertexShaderSource, fragmentShaderSource);
-
-    shProgram = new ShaderProgram('Phong', prog);
-    shProgram.Use();
-
-    shProgram.iAttribVertex   = gl.getAttribLocation(prog, "vertex");
-    shProgram.iAttribNormal   = gl.getAttribLocation(prog, "normal");
-    shProgram.iAttribUV       = gl.getAttribLocation(prog, "uv");
-    shProgram.iAttribTangent  = gl.getAttribLocation(prog, "tangent");
-    shProgram.iAttribBitangent= gl.getAttribLocation(prog, "bitangent");
-
-    shProgram.iModelViewProjectionMatrix = gl.getUniformLocation(prog, "ModelViewProjectionMatrix");
-    shProgram.iModelViewMatrix           = gl.getUniformLocation(prog, "ModelViewMatrix");
-    shProgram.iNormalMatrix              = gl.getUniformLocation(prog, "NormalMatrix");
-
-    shProgram.iColor     = gl.getUniformLocation(prog, "color");
-    shProgram.iLightPos  = gl.getUniformLocation(prog, "lightPos");
-    shProgram.iKa        = gl.getUniformLocation(prog, "Ka");
-    shProgram.iKd        = gl.getUniformLocation(prog, "Kd");
-    shProgram.iKs        = gl.getUniformLocation(prog, "Ks");
-    shProgram.iShininess = gl.getUniformLocation(prog, "shininess");
-
-    shProgram.iDiffuseTex  = gl.getUniformLocation(prog, "diffuseTex");
-    shProgram.iSpecularTex = gl.getUniformLocation(prog, "specularTex");
-    shProgram.iNormalTex   = gl.getUniformLocation(prog, "normalTex");
-
-    shProgram.iRotationAngle = gl.getUniformLocation(prog, "uRotationAngle");
-    shProgram.iRotationCenter = gl.getUniformLocation(prog, "uRotationCenter");
-
-    gl.enable(gl.DEPTH_TEST);
-    gl.depthFunc(gl.LEQUAL);
-
-    buildSurface();
-
-    // Load textures
-    diffuseTexture  = loadTexture("Textures/diffuse.jpg");
-    specularTexture = loadTexture("Textures/specular.jpg");
-    normalTexture   = loadTexture("Textures/normal.jpg");
 }
 
 function buildSurface() {
@@ -193,91 +104,168 @@ function buildSurface() {
     surface.init();
 }
 
-function createProgram(gl, vShader, fShader) {
-    let vsh = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vsh, vShader);
-    gl.compileShader(vsh);
-    if (!gl.getShaderParameter(vsh, gl.COMPILE_STATUS)) {
-        throw new Error("Error in vertex shader:  " + gl.getShaderInfoLog(vsh));
-    }
-    let fsh = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(fsh, fShader);
-    gl.compileShader(fsh);
-    if (!gl.getShaderParameter(fsh, gl.COMPILE_STATUS)) {
-        throw new Error("Error in fragment shader:  " + gl.getShaderInfoLog(fsh));
-    }
-    let prog = gl.createProgram();
-    gl.attachShader(prog, vsh);
-    gl.attachShader(prog, fsh);
-    gl.linkProgram(prog);
-    if (!gl.getProgramParameter(prog, gl.LINK_STATUS)) {
-        throw new Error("Link error in program:  " + gl.getProgramInfoLog(prog));
-    }
-    return prog;
-}
-
-function init() {
-    let canvas;
-    zoom = 1;
-
-    canvas = document.getElementById("webglcanvas");
-    gl = canvas.getContext("webgl");
-    if (!gl) {
-        document.getElementById("canvas-holder").innerHTML =
-            "<p>Sorry, could not get a WebGL graphics context.</p>";
+function draw() {
+    if (texturesLoaded < 3) {
+        requestAnimationFrame(draw);
         return;
     }
 
-    uSlider = document.getElementById("uSlider");
-    vSlider = document.getElementById("vSlider");
+    gl.clearColor(0,0,0,1);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    uSlider.oninput = function() {
-        buildSurface();
-    };
-    vSlider.oninput = function() {
-        buildSurface();
+    let projection = m4.perspective(Math.PI/8, 1, 8, 50);
+    let modelView = spaceball.getViewMatrix();
+
+    let rotX = m4.xRotation((270 * Math.PI) / 180);
+    let rotY = m4.yRotation((45 * Math.PI) / 180);
+    modelView = m4.multiply(rotX, modelView);
+    modelView = m4.multiply(rotY, modelView);
+
+    let translateBack = m4.translation(0, 0, -15 / zoom);
+    modelView = m4.multiply(translateBack, modelView);
+
+    lightAngle += 0.01;
+    let lx = 5.0 * Math.cos(lightAngle);
+    let ly = 4.0;
+    let lz = 5.0 * Math.sin(lightAngle);
+    let lightPos = [lx, ly, lz];
+    let lightEye = m4.transformPoint(modelView, lightPos);
+
+    let modelViewProjection = m4.multiply(projection, modelView);
+
+    let normalMatrix = m4.inverse(modelView);
+    normalMatrix = m4.transpose(normalMatrix);
+    let normalMatrix3 = [
+        normalMatrix[0], normalMatrix[1], normalMatrix[2],
+        normalMatrix[4], normalMatrix[5], normalMatrix[6],
+        normalMatrix[8], normalMatrix[9], normalMatrix[10]
+    ];
+
+    shProgram.Use();
+
+    gl.uniformMatrix4fv(shProgram.iModelViewProjectionMatrix, false, modelViewProjection);
+    gl.uniformMatrix4fv(shProgram.iModelViewMatrix,           false, modelView);
+    gl.uniformMatrix3fv(shProgram.iNormalMatrix,              false, normalMatrix3);
+
+    gl.uniform4fv(shProgram.iColor,    [1,1,1,1]);
+    gl.uniform3fv(shProgram.iLightPos, lightEye);
+    gl.uniform3fv(shProgram.iKa,       [0.2, 0.2, 0.2]);
+    gl.uniform3fv(shProgram.iKd,       [0.8, 0.8, 0.7]);
+    gl.uniform3fv(shProgram.iKs,       [1.0, 1.0, 1.0]);
+    gl.uniform1f(shProgram.iShininess, 50.0);
+
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, diffuseTexture);
+    gl.uniform1i(shProgram.iDiffuseTex, 0);
+
+    gl.activeTexture(gl.TEXTURE1);
+    gl.bindTexture(gl.TEXTURE_2D, specularTexture);
+    gl.uniform1i(shProgram.iSpecularTex, 1);
+
+    gl.activeTexture(gl.TEXTURE2);
+    gl.bindTexture(gl.TEXTURE_2D, normalTexture);
+    gl.uniform1i(shProgram.iNormalTex, 2);
+
+    gl.uniform2f(shProgram.iTexPivot, highlightU, highlightV);
+    gl.uniform1f(shProgram.iTexScale, texScaleValue);
+
+    gl.uniform2f(shProgram.iHighlightUV, highlightU, highlightV);
+
+    surface.draw(shProgram);
+
+    requestAnimationFrame(draw);
+}
+
+function initGL() {
+    let prog = createProgram(gl, vertexShaderSource, fragmentShaderSource);
+    shProgram = new ShaderProgram(prog);
+    shProgram.Use();
+
+    shProgram.iAttribVertex    = gl.getAttribLocation(prog, 'vertex');
+    shProgram.iAttribNormal    = gl.getAttribLocation(prog, 'normal');
+    shProgram.iAttribUV        = gl.getAttribLocation(prog, 'uv');
+    shProgram.iAttribTangent   = gl.getAttribLocation(prog, 'tangent');
+    shProgram.iAttribBitangent = gl.getAttribLocation(prog, 'bitangent');
+
+    shProgram.iModelViewProjectionMatrix = gl.getUniformLocation(prog, 'ModelViewProjectionMatrix');
+    shProgram.iModelViewMatrix           = gl.getUniformLocation(prog, 'ModelViewMatrix');
+    shProgram.iNormalMatrix              = gl.getUniformLocation(prog, 'NormalMatrix');
+    shProgram.iColor                     = gl.getUniformLocation(prog, 'color');
+    shProgram.iLightPos                  = gl.getUniformLocation(prog, 'lightPos');
+    shProgram.iKa                        = gl.getUniformLocation(prog, 'Ka');
+    shProgram.iKd                        = gl.getUniformLocation(prog, 'Kd');
+    shProgram.iKs                        = gl.getUniformLocation(prog, 'Ks');
+    shProgram.iShininess                 = gl.getUniformLocation(prog, 'shininess');
+    shProgram.iDiffuseTex                = gl.getUniformLocation(prog, 'diffuseTex');
+    shProgram.iSpecularTex               = gl.getUniformLocation(prog, 'specularTex');
+    shProgram.iNormalTex                 = gl.getUniformLocation(prog, 'normalTex');
+    shProgram.iTexPivot                  = gl.getUniformLocation(prog, 'texPivot');
+    shProgram.iTexScale                  = gl.getUniformLocation(prog, 'texScale');
+    shProgram.iHighlightUV               = gl.getUniformLocation(prog, 'highlightUV');
+
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+
+    buildSurface();
+
+    // Load textures
+    diffuseTexture  = loadTexture("Textures/diffuse.jpg");
+    specularTexture = loadTexture("Textures/specular.jpg");
+    normalTexture   = loadTexture("Textures/normal.jpg");
+}
+
+function init() {
+    let canvas = document.getElementById('webglcanvas');
+    gl = canvas.getContext('webgl');
+    if (!gl) {
+        document.getElementById('canvas-holder').innerHTML =
+            "<p>Sorry, could not get a WebGL context.</p>";
+        return;
+    }
+
+    uSlider = document.getElementById('uSlider');
+    vSlider = document.getElementById('vSlider');
+    scaleSlider = document.getElementById('scaleSlider');
+
+    uSlider.oninput = () => { buildSurface(); };
+    vSlider.oninput = () => { buildSurface(); };
+    scaleSlider.oninput = () => {
+        texScaleValue = parseFloat(scaleSlider.value) / 100.0;
     };
 
     initGL();
     spaceball = new TrackballRotator(canvas, null, 0);
 
-    // Zoom event
-    canvas.addEventListener('wheel', function(event) {
-        event.preventDefault();
-        zoom += event.deltaY * -0.001;
+    // Zoom with mouse wheel
+    canvas.addEventListener('wheel', function(ev) {
+        ev.preventDefault();
+        zoom += ev.deltaY * -0.001;
         zoom = Math.min(Math.max(zoom, 0.1), 2);
     });
 
-    window.addEventListener("keydown", function(e) {
-        let step = 0.01;
-        switch(e.key) {
+    window.addEventListener('keydown', (ev) => {
+        switch (ev.key) {
             case 'a':
             case 'A':
-                uRotationCenter[0] += step;
-                if(uRotationCenter[0] > 1.0) uRotationCenter[0] = 1.0;
+                highlightU += highlightStep;
+                if (highlightU > 1.0) highlightU = 1.0;
                 break;
             case 'd':
             case 'D':
-                uRotationCenter[0] -= step;
-                if(uRotationCenter[0] < 0.0) uRotationCenter[0] = 0.0;
+                highlightU -= highlightStep;
+                if (highlightU < 0.0) highlightU = 0.0;
                 break;
             case 'w':
             case 'W':
-                uRotationCenter[1] += step;
-                if(uRotationCenter[1] > 1.0) uRotationCenter[1] = 1.0;
+                highlightV += highlightStep;
+                if (highlightV > 1.0) highlightV = 1.0;
                 break;
             case 's':
             case 'S':
-                uRotationCenter[1] -= step;
-                if(uRotationCenter[1] < 0.0) uRotationCenter[1] = 0.0;
+                highlightV -= highlightStep;
+                if (highlightV < 0.0) highlightV = 0.0;
                 break;
-            case 'q':
-            case 'Q':
-                uRotationAngle += 0.05;
-                break;
-            case 'e':
-            case 'E':
-                uRotationAngle -= 0.05;
+            default:
                 break;
         }
     });
--- shader.glsl
+++ shader.glsl
@@ -9,106 +9,107 @@ uniform mat4 ModelViewProjectionMatrix;
 uniform mat4 ModelViewMatrix;
 uniform mat3 NormalMatrix;
 
+uniform float texScale;
+uniform vec2 texPivot;
+
 varying vec2 vTexCoord;
-varying vec3 vT; // tangent in eye space
-varying vec3 vB; // bitangent in eye space
-varying vec3 vN; // normal in eye space
+varying vec2 vOrigUV;  // the original (u,v), for highlight logic
 varying vec3 vPosition;
+varying vec3 vT;  // tangent in eye space
+varying vec3 vB;  // bitangent in eye space
+varying vec3 vN;  // normal in eye space
 
 void main(void) {
     vec4 posEye = ModelViewMatrix * vec4(vertex, 1.0);
     vPosition = posEye.xyz;
 
-    // Prioritize normal vector in Gram-Schmidt
     vec3 N = normalize(NormalMatrix * normal);
-    // Orthogonalize tangent
-    vec3 T = tangent - dot(tangent, N)*N;
-    T = normalize(T);
-    T = NormalMatrix * T;
 
-    // Bitangent using cross
+    // Step 1: project tangent away from N
+    vec3 T = tangent - dot(tangent, N)*N;
+    T = NormalMatrix * T;
+    T = normalize(T);
+
     vec3 B = NormalMatrix * bitangent;
-    // Recompute if needed to ensure orthogonality
     B = normalize(cross(N, T));
 
     vN = N;
     vT = T;
     vB = B;
 
-    vTexCoord = uv;
+    vOrigUV = uv;
+
+    // Texture scaling around pivot
+    vec2 shifted = uv - texPivot;
+    shifted *= texScale;
+    vTexCoord = shifted + texPivot;
 
     gl_Position = ModelViewProjectionMatrix * vec4(vertex, 1.0);
 }
 `;
 
 const fragmentShaderSource = `#version 100
-precision mediump float;                                  
-                                                          
+precision mediump float;
+
 varying vec2 vTexCoord;
-varying vec3 vT;                                          
-varying vec3 vB;                                          
-varying vec3 vN;                                          
-varying vec3 vPosition;                                   
-                                                          
+varying vec2 vOrigUV;
+varying vec3 vPosition;
+varying vec3 vT;
+varying vec3 vB;
+varying vec3 vN;
+
 uniform sampler2D diffuseTex;
-uniform sampler2D specularTex;                            
-uniform sampler2D normalTex;                              
-                                                          
+uniform sampler2D specularTex;
+uniform sampler2D normalTex;
+
 uniform vec4 color;
-uniform vec3 lightPos;                                    
-uniform vec3 Ka;                                          
-uniform vec3 Kd;                                          
-uniform vec3 Ks;                                          
-uniform float shininess;                                  
-                                                          
-uniform float uRotationAngle;
-uniform vec2 uRotationCenter;                             
-                                                          
-vec2 rotateUV(vec2 uv, vec2 center, float angle) {
-    vec2 temp = uv - center;
+uniform vec3 lightPos;
+uniform vec3 Ka;
+uniform vec3 Kd;
+uniform vec3 Ks;
+uniform float shininess;
 
-    float c = cos(angle);
-    float s = sin(angle);                                 
-    mat2 R = mat2(c, -s, s, c);
+uniform vec2 highlightUV;
 
-    temp = R * temp;
-    return temp + center;                                 
-}                                                         
-                                                          
-void main(void) {                                         
-    mat3 TBN = mat3(normalize(vT), normalize(vB), normalize(vN));
-                                                          
-    vec2 uv_rot = rotateUV(vTexCoord, uRotationCenter, uRotationAngle);
-                                                          
-    vec3 N_tangent = texture2D(normalTex, uv_rot).rgb;
-    N_tangent = 2.0 * N_tangent - 1.0;  // Range from [0,1]->[-1,1]      
-    vec3 N = normalize(TBN * N_tangent);                   
-                                                          
-    vec3 diffColor = texture2D(diffuseTex, uv_rot).rgb;
-    // Specular factor                                    
-    float specFactor = texture2D(specularTex, uv_rot).r;   
-                                                          
+void main(void) {
+    vec3 Nn = normalize(vN);
+    vec3 Tn = normalize(vT);
+    vec3 Bn = normalize(vB);
+    mat3 TBN = mat3(Tn, Bn, Nn);
+
+    vec3 normalSample = texture2D(normalTex, vTexCoord).rgb;
+    normalSample = 2.0 * normalSample - 1.0;
+    vec3 N = normalize(TBN * normalSample);
+
+    vec3 diffColor = texture2D(diffuseTex, vTexCoord).rgb;
+    float specFactor = texture2D(specularTex, vTexCoord).r;
+
     vec3 L = normalize(lightPos - vPosition);
+    vec3 V = vec3(0.0, 0.0, 1.0);
+    vec3 R = reflect(-L, N);
 
-    vec3 V = vec3(0,0,1);
-    vec3 R = reflect(-L, N);                               
-                                                          
-    float diffuse = max(dot(N, L), 0.0);                   
-    float spec = 0.0;                                      
-    if (diffuse > 0.0) {                                  
-        spec = pow(max(dot(R,V), 0.0), shininess) * specFactor; 
-    }                                                     
-                                                          
-    vec3 ambient = Ka;                                    
-    vec3 diff = Kd * diffColor * diffuse;                 
-    vec3 specular = Ks * spec;                            
-    vec3 finalColor = ambient + diff + specular;
-
-    float d = distance(vTexCoord, uRotationCenter);
-    if(d < 0.01) {
-       finalColor = mix(finalColor, vec3(1.0,0.0,0.0), 0.8);
+    float diffuse = max(dot(N, L), 0.0);
+    float spec = 0.0;
+    if(diffuse > 0.0) {
+        spec = pow(max(dot(R, V), 0.0), shininess) * specFactor;
     }
 
-    gl_FragColor = vec4(finalColor, 1.0) * color;
-}                                                         
+    vec3 ambient = Ka;
+    vec3 diff    = Kd * diffColor * diffuse;
+    vec3 specular= Ks * spec;
+
+    vec3 finalRGB = ambient + diff + specular;
+
+    // Base output color
+    vec4 outColor = vec4(finalRGB, 1.0) * color;
+
+    float distUV = distance(vOrigUV, highlightUV);
+    if(distUV < 0.01) {
+        float highlightStrength = 0.9;
+        vec4 highlightColor = vec4(1.0, 0.0, 0.0, 1.0);
+        outColor = mix(outColor, highlightColor, highlightStrength);
+    }
+
+    gl_FragColor = outColor;
+}
 `;
